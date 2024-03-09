3767178989206aa8c13dfbc8df68281262c74b97
Mitchel Kelonye <kelonyemitchel@gmail.com>
2024-03-09 20:37:16 +0400

    init

---
+++ script.ts
@@ -0,0 +1,93 @@
+import * as bip39 from 'bip39';
+import * as bip32 from 'bip32';
+import { derivePath } from 'ed25519-hd-key';
+import nacl from 'tweetnacl';
+import { Keypair } from '@solana/web3.js';
+import base58 from 'bs58';
+
+const DerivationPath: { [key: string]: DerivationPath } = {
+  Bip44: 'bip44',
+  Bip44Change: 'bip44-change',
+  SolletDeprecated: 'sollet-deprecated',
+  Default: 'bip44',
+};
+
+type DerivationPath = 'bip44' | 'bip44-change' | 'sollet-deprecated';
+
+const MNEMONIC = process.env.MNEMONIC!;
+
+main().then(
+  () => process.exit(),
+  (err: Error) => {
+    console.log(err);
+    process.exit(-1);
+  },
+);
+
+async function main() {
+  derive(MNEMONIC, DerivationPath.Bip44Change, 1);
+}
+
+function derive(mnemonic: string, dpath: DerivationPath, count: number) {
+  const seed = bip39.mnemonicToSeedSync(mnemonic, '');
+  for (let i = 0; i < count; i++) {
+    for (const kp of deriveSolanaKeypairs(seed, dpath, [i])) {
+      console.log(kp.publicKey.toBase58(), base58.encode(kp.secretKey));
+    }
+  }
+}
+
+export function deriveSolanaKeypairs(
+  seed: Buffer,
+  derivationPath: DerivationPath,
+  accountIndices: Array<number>,
+): Array<Keypair> {
+  const kps: Array<Keypair> = [];
+  const seedHex = seed.toString('hex');
+  for (const accountIndex of accountIndices) {
+    const kp = deriveSolanaKeypair(seedHex, accountIndex, derivationPath);
+    kps.push(kp);
+  }
+  return kps;
+}
+
+// Returns the account Keypair for the given seed and derivation path.
+export function deriveSolanaKeypair(
+  seedHex: string,
+  accountIndex: number,
+  derivationPath: DerivationPath,
+): Keypair {
+  let dSeed = deriveSeed(seedHex, derivationPath, accountIndex);
+  const secret = nacl.sign.keyPair.fromSeed(dSeed).secretKey;
+  return Keypair.fromSecretKey(secret);
+}
+
+function deriveSeed(
+  seedHex: string,
+  derivationPath: DerivationPath,
+  accountIndex: number,
+): any {
+  const pathStr = derivePathStr(derivationPath, accountIndex);
+  switch (derivationPath) {
+    case DerivationPath.SolletDeprecated:
+      return bip32.fromSeed(Buffer.from(seedHex, 'hex')).derivePath(pathStr)
+        .privateKey;
+    default:
+      return derivePath(pathStr, seedHex).key;
+  }
+}
+
+function derivePathStr(derivationPath: DerivationPath, accountIndex: number) {
+  switch (derivationPath) {
+    case DerivationPath.Bip44:
+      return accountIndex === 0
+        ? `m/44'/501'`
+        : `m/44'/501'/${accountIndex - 1}'`;
+    case DerivationPath.Bip44Change:
+      return `m/44'/501'/${accountIndex}'/0'`;
+    case DerivationPath.SolletDeprecated:
+      return `m/501'/${accountIndex}'/0/0`;
+    default:
+      throw new Error(`invalid derivation path: ${derivationPath}`);
+  }
+}
